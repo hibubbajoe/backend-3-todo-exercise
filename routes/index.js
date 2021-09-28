63d307c588b28e01d3a2988b5e8bfa7ee3c9c18b
Robin Hedlund <robin.live@live.se>
2021-09-28 11:22:24 +0200

    added items array

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+const items = [
+  {
+    id: 1,
+    title: 'Clean youself',
+    body: 'Shower, shave and misbehave'
+  },
+  {
+    id: 2,
+    title: 'Buy LEGO',
+    body: 'Deathstar pls'
+  },
+  {
+    id: 3,
+    title: 'Meditate',
+    body: 'meditate before you medicate'
+  }
+]
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Todo', items: items });
 });
 
 module.exports = router;
