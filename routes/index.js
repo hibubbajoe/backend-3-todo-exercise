fe6b503a21ac88ae8f999c424a120fe89354a4bb
Robin Hedlund <robin.live@live.se>
2021-09-28 13:53:42 +0200

    add update todo route and view

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const { title } = require('process');
 var router = express.Router();
 
 const items = [
@@ -23,5 +24,25 @@ const items = [
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Todo', items: items });
 });
+
+/* UPDATE ITEM */
+router.get('/item/:id', function (req, res, next) {
+  const itemId = parseInt(req.params.id);
+  const item = items.find(item => item.id === itemId);
+  res.render('item', { item: item });
+
+
+});
+
+router.post('/item/:id', function (req, res, next) {
+  const itemId = parseInt(req.params.id);
+  const item = items.find(item => item.id === itemId);
+  const { title, body } = req.body;
+  item.title = title;
+  item.body = body;
+
+  res.redirect('/');
+})
+
 
 module.exports = router;
